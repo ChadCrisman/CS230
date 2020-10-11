dfe867258cf5b4916124845bdecdc61881cb0135
Chad <cc0089@mix.wvu.edu>
2020-10-11 19:47:27 -0400

    The working Lab 6 code

--- gallery.php
+++ gallery.php
@@ -5,28 +5,37 @@ require 'includes/header.php';
 <main>
 <link rel="stylesheet" href="css/gallery.css">
     <h1>Gallery</h1>
-        <div class="gallery-container">
-            <?php 
-                include_once 'includes/dbhandler.php';
-                $sql = "SELECT * FROM products ORDER BY upload_date DESC";
-                $stmt = mysqli_stmt_init($conn);
+        <div class = "gallery-container">
+            <?php
+            include_once 'includes/dbhandler.php';
+            $sql = "SELECT * FROM products ORDER BY upload_date DESC";
+            $stmt = mysqli_stmt_init($conn);
+            if(!mysqli_stmt_prepare($stmt, $sql))
+            {
+                echo 'SQL Failure';
 
-                if (!mysqli_stmt_prepare($stmt, $sql)) {
-                    echo 'SQL failure';
+            }
+            else{
+
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    echo '<div class = "card">
+                        <a href = "#">
+                            <img src = "IMAGES/'.$row["picpath"].'">
+                            <h3>'.$row["title"].'</h3>
+                            <p>'.$row["descript"].'</p>
+                        
+                        </a>
+
+                     </div>';
                 }
-                else {
-                    mysqli_stmt_execute($stmt);
-                    $result = mysqli_stmt_get_result($stmt);
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo  '<div class="card">
-                            <a href="#">
-                                <img src="product/'.$row["picpath"].'">
-                                <h3>'.$row["title"].'</h3>
-                                <p>'.$row["descript"].'</p>
-                            </a>
-                        </div>';
-                    }
-                }
+
+            }
+
             ?>
         </div>
+    
+
 </main>
--- includes/gallery-helper.php
+++ includes/gallery-helper.php
@@ -1,68 +1,61 @@
 <?php 
 
-function console_log($output, $with_script_tags = true) {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
-');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
-}
-
 require 'dbhandler.php';
 define('MB', 1048576);
 
-if (isset($_POST['gallery-submit'])) {
-    console_log($_FILES);
-    $file = $_FILES['gallery-image'];
+if(isset($_POST['gallery-submit'])){
 
+    $file = $_FILES['gallery-image'];
     $file_name = $file['name'];
     $file_tmp_name = $file['tmp_name'];
     $file_error = $file['error'];
     $file_size = $file['size'];
-
     $title = $_POST['title'];
     $descript = $_POST['descript'];
-
+    $new_name = $_POST['new_name'];
 
     $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
-    $allowed = array('jpg','jpeg','png','svg','jfif');
+    $allowed = array('jpg', 'jpeg', 'png', 'svg', "jfif");
 
-    if ($file_error !== 0) {
-        header("Location: ../admin.php?error=UpdateError");
+    if($file_error !== 0)
+    {
+        header("Location: ../admin.php?error = UploadError");
         exit();
     }
-     if (!in_array($ext, $allowed)) {
-        header("Location: ../admin.php?error=invalidType");
+    if(!in_array($ext, $allowed))
+    {
+        header("Location: ../admin.php?error = InvalidType");
         exit();
-   }
-    if ($file_size > 4*MB) {
-        header("Location: ../admin.php?error=fileSizeToLarge");
+    }
+    if($file_size > 4*MB)
+    {
+        header("Location: ../admin.php?error = FileSizeExceeded");
         exit();
     }
     else{
-        $new_name = uniqid('',true).".".$ext;
+
+        $new_name = uniqid('', true).".".$ext;
         $destination = '../Products/'.$new_name;
 
-        $sql = "INSERT INTO products (title, descript, picpath) VALUES (?,?,?)"; 
+        $sql = "INSERT INTO products (title, descript, picpath) VALUES(?,?,?)";
         $stmt = mysqli_stmt_init($conn);
 
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            header("Location: ../login.php?error=sqlInjection");
+        if(!mysqli_stmt_prepare($stmt, $sql))
+        {
+            header("Location: ../login.php?error = SQLInjection");
             exit();
         }
         else{
-            mysqli_stmt_bind_param($stmt,"sss", $title, $descript, $new_name);
+            mysqli_stmt_bind_param($stmt, "sss", $title, $descript, $new_name);
             mysqli_stmt_execute($stmt);
             mysqli_stmt_store_result($stmt);
 
-            move_uploaded_file($file_tmp_name,$destination);
-            header("Location: ../admin.php?success=updateSuccess");
+            move_uploaded_file($file_tmp_name, $destination);
+            header("Location: ../admin.php?success = UploadSuccess");
             exit();
         }
     }
 }
-
 ?>
 

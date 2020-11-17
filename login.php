bfb7867bb059fdbefb3244b698e0c5daa69640b5
Chad <cc0089@mix.wvu.edu>
2020-11-17 13:27:29 -0500

    Updated Carousel to remove arrows and indicators

--- login.php
+++ login.php
@@ -5,11 +5,6 @@ require 'includes/header.php'
 <link rel="stylesheet" href="css/login.css">
     <div class="background">
         <div id="carouselIndicators" class="carousel-slide" data-ride="carousel" style="margin-top: 80px">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselIndicators" data-slide-to="2"></li>
-            </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img src="images/carousel1.jpg" class="d-block " alt="First Image">
@@ -21,14 +16,6 @@ require 'includes/header.php'
                     <img src="images/carousel4.jpg" class="d-block " alt="Third Image">
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
         <div class="back-screen">
             <div class="h-40 center-me">
